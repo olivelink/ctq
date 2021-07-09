c0b00b4ff1bc6d46e2c2c7fa7976cba79c8ece0c
Adam Terrey <arterrey@gmail.com>
2021-07-09 17:38:21 +0700

    Add iter_named_resources to exported functions

--- ctq/__init__.py
+++ ctq/__init__.py
@@ -4,6 +4,7 @@ from .events import emit
 from .events import handle
 from .named_resource import resource
 from .named_resource import Resourceful
+from .named_resource import iter_named_resources
 from .resource_cache import ResourceCache
 from .traversal import get_root
 from .traversal import resource_path_names
